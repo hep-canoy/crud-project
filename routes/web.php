dd3d3914a8bb518e75e0f8cb615c26ab0d72a634
Hephzibah Canoy <hephzibahcanoy123@gmail.com>
2022-05-04 10:44:12 +0800

    changed the route

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,12 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('/products', ProductController::class);
 
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-Route::post('/products/create', [ProductController::class, 'store'])->name('products.store');
-Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
-Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
-Route::get('/products/edit/{id}', [ProductController::class, 'edit'])->name('products.edit');
-Route::put('/products/edit/{id}', [ProductController::class, 'update'])->name('products.update');
+// Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+// Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+// Route::post('/products/create', [ProductController::class, 'store'])->name('products.store');
+// Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
+// Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
+// Route::get('/products/edit/{id}', [ProductController::class, 'edit'])->name('products.edit');
+// Route::put('/products/edit/{id}', [ProductController::class, 'update'])->name('products.update');
